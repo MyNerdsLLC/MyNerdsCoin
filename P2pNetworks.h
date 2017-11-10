63d7d2b1c694d8b4d82f4b320d1f0de23c25428c
MyNerdsLLC <33535241+MyNerdsLLC@users.noreply.github.com>
2017-11-09 19:39:28 -0800

    Add files via upload

---
+++ P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x21, 0x93, 0x65, 0x31, 0x02, 0xd9, 0x6h, 0xt6, 0xh4, 0x3g, 0x0f, 0x9t, 0xj8, 0x9g, 0x84, 0x7f } };
+}
